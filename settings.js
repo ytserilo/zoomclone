9ab349337a0292cb8c7e983ddf911002aaa9b7ab
Yarik <acemirskija@gmail.com>
2021-08-29 19:08:58 +0300

    start

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 const redis = require("redis");
-module.exports = redis.createClient({
-  host: "127.0.0.1",
-  port: 11503,
-  password: "2ab78279e9760e5bc722022ef6f78f69",
-});
+// module.exports = redis.createClient({
+//   host: "127.0.0.1",
+//   port: 11503,
+//   password: "2ab78279e9760e5bc722022ef6f78f69",
+// });
